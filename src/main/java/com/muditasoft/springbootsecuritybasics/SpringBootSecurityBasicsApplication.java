3e6080e4da329e81b944d5d348a9a255313cb44d
tutkuince <tutku.ince@outlook.com>
2019-12-11 11:53:53 +0300

    initial commit

---
+++ src/main/java/com/muditasoft/springbootsecuritybasics/SpringBootSecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootsecuritybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muditasoft/springbootsecuritybasics/SpringBootSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
